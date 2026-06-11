1d196217a6faab7316f1bdd083afab0a0651385f
Hatice Eda Yılmaz <htcedayilmaz@gmail.com>
2026-06-11 19:03:49 +0300

    delete build_links.py, alter main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from read_data import download_all, read_career_files, read_course_files
+import argparse
+from read_data import download_all, read_career_files, read_course_file
+from linker import build_career_course_links
 
-download_all()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--build-links", action="store_true")
+    parser.add_argument("--build-graph", action="store_true")
+    parser.add_argument("--evaluate", action="store_true")
+    args = parser.parse_args()
 
-careers = read_career_files()
-courses = read_course_files()
+    download_all()
+    careers = read_career_files()
+    courses = read_course_file()
+
+    if args.build_links:
+        results = build_career_course_links(careers, courses)
+        triples = convert_to_triples(results)
+        save_results(triples, OUTPUT_PATH)
+
+    if args.build_graph:
+        pass
+
+    if args.evaluate:
+        pass
+if __name__ == "__main__":
+    main()
